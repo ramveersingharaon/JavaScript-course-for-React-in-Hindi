329f6c3c72f0d3c5844a927c8a656a040f2ae5aa
Ramveer Singh <ramveersingharaon@gmail.com>
2023-01-15 21:57:23 +0530

    In this tutorial I will tech you setTimeout and SetInterval in javascript

---
+++ Tutorial 22/index.js
@@ -0,0 +1,6 @@
+document.getElementById("btn").addEventListener("click",()=>{
+    console.log("Button was clicked")
+})
+document.getElementById("btn").addEventListener("mouseout",()=>{
+    console.log("mouse out")
+})
