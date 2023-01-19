6967f688e785231136b7147660d63f696ebd6fbf
Ramveer Singh <ramveersingharaon@gmail.com>
2023-01-19 22:04:33 +0530

    In this video I will teach you Module in javascript

---
+++ Tutorial 24/app.js
@@ -0,0 +1,11 @@
+// import { var2 as v } from "./user.js";
+// import func from "./user.js";
+
+
+import  * as user from "./user.js";
+
+console.log(user.var1)
+
+
+user.func1()
+user.func2()
---
+++ Tutorial 24/user.js
@@ -0,0 +1,12 @@
+export let var1 = "Ram";
+
+export let var2 = 22;
+
+
+export  function func1() {
+    console.log("Hello everyOne")
+}
+
+export function func2() {
+    console.log("Good Moring");
+}
